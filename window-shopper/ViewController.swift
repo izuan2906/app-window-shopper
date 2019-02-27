78ccbb4a6305d01193fac501ad80fa5a4908606f
Izuan Ismail <izuan2906@gmail.com>
2019-02-27 11:27:07 +0800

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Izuan Ismail on 27/2/19.
+//  Copyright © 2019 Da'ieMind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
